972df0a943e120b39dba8e59451fd8b6d576916e
David S Lakind <40665394+dslakind@users.noreply.github.com>
2022-08-05 14:01:56 -0400

    Create EvenOrOdd.java

---
+++ EvenOrOdd.java
@@ -0,0 +1,24 @@
+/*
+  # Instructions  
+  Complete the even_or_odd function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+*/
+public class EvenOrOdd {
+  
+  
+  public static String even_or_odd(int number) {
+      //Place your code here 
+
+  }
+  
+  // The main method will help you test your even_or_odd method for accuracy when you run the program.
+  public static void main(String[] args) {
+    EvenOrOdd eoo = new EvenOrOdd();
+    System.out.println(eoo.even_or_odd(2));
+    System.out.println(eoo.even_or_odd(6));
+    System.out.println(eoo.even_or_odd(7));
+    System.out.println(eoo.even_or_odd(0));
+    System.out.println(eoo.even_or_odd(8273));
+    System.out.println(eoo.even_or_odd(-35));
+    
+  }  
+}
